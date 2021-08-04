037140e28f5e5799577c7af09434c84fa58c6782
maxus <maksim_astapkovich@epam.com>
2021-08-04 21:53:37 +0300

    E

--- script.sh
+++ script.sh
@@ -33,11 +33,9 @@ cd InfrastructureJenkins/container
 
 var=`docker ps | grep jenkins | cut -d" " -f1`
 
-echo "$${var}" > ass 
+docker exec -it `docker ps | grep jenkins | cut -d" " -f1` apt-get update
 
-docker exec -it "$${var}" apt-get update
-
-docker exec -it "$${var}" apt-get install -y docker.io
+docker exec -it `docker ps | grep jenkins | cut -d" " -f1` apt-get install -y docker.io
 
 docker-compose up -d
 
